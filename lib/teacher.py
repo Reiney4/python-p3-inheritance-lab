8c5bd504d530e15e1b25438c9b7883509a766943
Valerie <valeriejeptoo4@gmail.com>
2023-08-24 13:10:25 +0300

    completed assignment

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,16 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name=None, last_name=None, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+
+# Instantiate a Teacher object with knowledge list
+my_teacher = Teacher("My", "Teacher", knowledge=[
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    # Add other knowledge strings here
+])
